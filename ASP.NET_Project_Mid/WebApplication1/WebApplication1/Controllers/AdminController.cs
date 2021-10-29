4e3ac6cc43733f30270a4847fe85aca7c8caba2b
HabiburRahmanAnik <habib264521@gmail.com>
2021-10-29 09:13:56 +0600

    add validation on add user form

--- ASP.NET_Project_Mid/WebApplication1/WebApplication1/Controllers/AdminController.cs
+++ ASP.NET_Project_Mid/WebApplication1/WebApplication1/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -16,8 +17,56 @@ namespace WebApplication1.Controllers
 
         public ActionResult UserList()
         {
+            HomeRentEntities db = new HomeRentEntities();
+            var user = from u in db.Users
+                       select u;
+
+            return View(user.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult AddUser()
+        {
             return View();
         }
-        
+
+        [HttpPost]
+        public ActionResult AddUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                HomeRentEntities db = new HomeRentEntities();
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("UserList");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            using(HomeRentEntities db = new HomeRentEntities())
+            {
+                var u = (from us in db.Users
+                         where us.UserId == id
+                         select us).First();
+                return View(u);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+           using (HomeRentEntities db = new HomeRentEntities())
+            {
+                User us = (from u in db.Users
+                          where u.UserId == id
+                          select u).FirstOrDefault();
+                db.Users.Remove(us);
+                db.SaveChanges();
+                return RedirectToAction("UserList");
+            }
+        }
+
     }
 }
